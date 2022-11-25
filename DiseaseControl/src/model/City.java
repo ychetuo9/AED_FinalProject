02141c1147843950e658c32c18fde99700207d82
Xiaodi Huang <huang.xiaod@northeastern.edu>
2022-11-24 19:42:37 -0500

    Initialize from Chen_Yanyan_002799697

---
+++ DiseaseControl/src/model/City.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author yanyanchen
+ */
+public class City {
+    String id;
+    String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
